53b8158c869406f1a2f846793941bee64ae02165
Manikandan k <168514765+KaviMani09@users.noreply.github.com>
2024-09-27 08:54:22 +0530

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
           <a
             data-aos="fade-up"
             data-aos-duration="1400"
-            href="https://www.linkedin.com/in /kavimanikandan09"
+            href="https://www.linkedin.com/in/kavimanikandan09"
             className="box font-medium text-white  flex items-start justify-end gap-1"
           >
             <FaLinkedinIn className=" text-white text-xl hover:scale-125 cursor-pointer" />
